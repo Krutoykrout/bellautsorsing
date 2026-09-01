ec8a818e435850dbbddaaf824c2aceb54a955d73
Krutoykrout <krout00@mail.ru>
2026-09-02 04:49:17 +0700

    Remove site files

--- js/types/grecaptcha.d.ts
+++
@@ -1,6 +0,0 @@
-declare var grecaptcha: {
-    execute(siteKey: string, options?: { action: string }): Promise<string>;
-    render(container: string | HTMLElement, parameters: any): number;
-    reset(widgetId?: number): void;
-    getResponse(widgetId?: number): string;
-};
